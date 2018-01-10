75953c1f992a750f2daca5fa0cbcfab69e032227
Antal Andrei <antal.andrei@icloud.com>
2018-01-10 11:43:20 +0200

    Start code for challange 8 - Multiple components

--- src/app/movies/movie.component.ts
+++ src/app/movies/movie.component.ts
@@ -6,33 +6,70 @@ import { Component } from '@angular/core';
   styleUrls: ['./movie.component.css']
 })
 export class MovieComponent {
-  public title = 'Star Wars: The Last Jedi';
-  public year = 2017;
-  public duration = '152 min';
-  public genre = 'Action, Adventure, Fantasy';
-  public plot = 'Rey develops her newly discovered abilities with the guidance of Luke Skywalker, who is unsettled by the strength of her powers. Meanwhile, the Resistance prepares to do battle with the First Order.';
-  public poster = 'https://images-na.ssl-images-amazon.com/images/M/MV5BMjQ1MzcxNjg4N15BMl5BanBnXkFtZTgwNzgwMjY4MzI@._V1_SX300.jpg';
 
-  public isLiked = false;
-  public isSaved = false;
-  public comment = '';
+  public movies = [
+    {
+      title: 'Star Wars: The Last Jedi',
+      year: 2017,
+      duration: '152 min',
+      genre: 'Action, Adventure, Fantasy',
+      plot: 'Rey develops her newly discovered abilities with the guidance of Luke Skywalker, who is unsettled by the strength of her powers. Meanwhile, the Resistance prepares to do battle with the First Order.',
+      poster: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMjQ1MzcxNjg4N15BMl5BanBnXkFtZTgwNzgwMjY4MzI@._V1_SX300.jpg',
+      isLiked: false,
+      commentSaved: false,
+      comment: '',
+    },
+    {
+      title: 'Black Swan',
+      year: 2010,
+      duration: '108 min',
+      genre: 'Drama, Thriller',
+      plot: 'A committed dancer wins the lead role in a production of Tchaikovskys \"Swan Lake\" only to find herself struggling to maintain her sanity.',
+      poster: 'https://images-na.ssl-images-amazon.com/images/M/MV5BNzY2NzI4OTE5MF5BMl5BanBnXkFtZTcwMjMyNDY4Mw@@._V1_SX300.jpg',
+      isLiked: false,
+      commentSaved: false,
+      comment: '',
+    },
+    {
+      title: 'Fight Club',
+      year: 1999,
+      duration: '139 min',
+      genre: 'Drama',
+      plot: 'An insomniac office worker, looking for a way to change his life, crosses paths with a devil-may-care soapmaker, forming an underground fight club that evolves into something much, much more.',
+      poster: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMzFjMWNhYzQtYTIxNC00ZWQ1LThiOTItNWQyZmMxNDYyMjA5XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg',
+      isLiked: false,
+      commentSaved: false,
+      comment: '',
+    },
+    {
+      title: 'The Godfather: Part II',
+      year: 1974,
+      duration: '202 min',
+      genre: 'Crime, Drama',
+      plot: 'The early life and career of Vito Corleone in 1920s New York City is portrayed, while his son, Michael, expands and tightens his grip on the family crime syndicate.',
+      poster: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMjZiNzIxNTQtNDc5Zi00YWY1LThkMTctMDgzYjY4YjI1YmQyL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg',
+      isLiked: false,
+      commentSaved: false,
+      comment: '',
+    }
+  ];
 
-  like() {
-    this.isLiked = !this.isLiked;
+  like(index) {
+    this.movies[index].isLiked = !this.movies[index].isLiked;
   }
 
-  clear() {
-    this.comment = '';
+  clear(index) {
+    this.movies[index].comment = '';
   }
 
-  save() {
-    this.isSaved = true;
+  save(index) {
+    this.movies[index].commentSaved = true;
   }
 
-  getWords() {
-    if (this.comment === '') {
+  getWords(newComment) {
+    if (newComment === '') {
       return 0;
     }
-    return this.comment.trim().split(' ').length;
+    return newComment.trim().split(' ').length;
   }
 }
