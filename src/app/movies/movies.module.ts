53034d92f40e42d8e325939a2feda7c66f722209
Antal Andrei <antal.andrei@icloud.com>
2018-01-10 11:56:43 +0200

    Start code for challange 9 - Component communication

--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { MovieListComponent } from './movieList.component';
 import { MovieComponent } from './movie.component';
 
 @NgModule({
   imports: [ BrowserModule, FormsModule ],
-  exports: [ MovieComponent ],
-  declarations: [ MovieComponent ],
+  exports: [ MovieListComponent ],
+  declarations: [ MovieComponent, MovieListComponent ],
   providers: [],
 })
 export class MoviesModule { }
